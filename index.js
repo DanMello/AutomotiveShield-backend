1780f943270e01a67c3784fed27d51301db75df4
Dan Mello <jdanmello@gmail.com>
2020-04-19 03:07:22 -0400

    added more routes to allow file uploading

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 require('dotenv').config()
 
+const env = process.env.NODE_ENV === "production" ? "production" : "development";
+const tmpFolder = env === "production" ? '../assets/tmp/' : './public/tmp/';
 const express = require("express");
-const nodemailer = require("nodemailer");
-const {connection} = require('./mongodb');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { connection } = require('./mongodb');
+const multer = require('multer');
+const ffmpeg = require('./ffmpeg');
+const storage = require('./multer')(multer, tmpFolder);
+const {transporter} = require('./nodemailer');
+const Jimp = require('jimp');
+const uuid = require('uuid');
 const tokenSecret = process.env.TOKEN_SECRET;
 const app = express();
-
-const settings = {
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASSWORD
-  }
-};
-const transporter = nodemailer.createTransport(settings);
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -27,6 +23,12 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
+app.use(express.urlencoded({extended: true})); 
+
+if (env === 'development') {
+  app.use(express.static('public'));
+};
 
 app.post('/api/contact', (req, res) => {
   const {firstName, lastName, email, message} = req.body;
@@ -142,7 +144,7 @@ app.post('/api/logout', (req, res) => {
       message: err.message
     });
   });
-})
+});
 
 app.post('/api/checkToken', (req, res) => {
   const { token } = req.body;
@@ -315,6 +317,220 @@ app.post('/api/updateemail', (req, res) => {
       token: newToken,
       email: email,
       message: 'Email updated successfully'
+    });
+  };
+
+  runAsync().catch(err => {
+    res.json({
+      error: true,
+      message: err.message
+    })
+  });
+});
+
+app.post('/api/uploadwork', (req, res) => {
+  const upload = storage.array('file', 12);
+  upload(req, res, function (err) {
+    if ((err instanceof multer.MulterError) || err) {
+      res.json({
+        error:true,
+        message: err.message
+      })
+      return;
+    };
+
+    function createScreenShot(video) {
+      return new Promise((resolve, reject) => {
+        const filename = uuid.v4();
+        const thumbnailName = `thumbnail-${filename}.png`;
+        ffmpeg(tmpFolder + video)
+        .on('end', function() {
+          resolve(thumbnailName)
+        })
+        .on('error', function(err) {
+          reject(err);
+        })
+        .screenshots({
+          timestamps: [1],
+          filename: thumbnailName,
+          folder: tmpFolder
+        });
+      })
+    };
+
+    function minifyImage(image) {
+      return new Promise((resolve, reject) => {
+        const filePath = tmpFolder + image;
+        Jimp.read(filePath).then(picture => {
+          return picture
+            .quality(50)
+            .write(filePath)
+        }).then(() => {
+          resolve();
+        }).catch(err => {
+          reject(err);
+        });
+      });
+    };
+
+    function handleUploads() {
+
+      const videoTypes = ['quicktime', 'mp4'];
+      const photoTypes = ['jpg', 'jpeg', 'png'];
+      return new Promise(async (resolve, reject) => {
+        const files = await Promise.all(req.files.map(async (file) => {
+          if ((videoTypes.indexOf(file.mimetype.split('/').pop()) > -1 )) {
+            const thumbnailName = await createScreenShot(file.filename).catch(err => reject(err));
+            file.thumbnailName = thumbnailName;
+          } else if ((photoTypes.indexOf(file.mimetype.split('/').pop()) > -1 )) {
+            await minifyImage(file.filename).catch(err => reject(err));
+          }
+          return file;
+        }));
+        resolve(files);
+      });
+    };
+
+    async function runAsync() {
+      const files = await handleUploads();
+      res.json(files);
+    };
+
+    runAsync().catch(err => {
+      res.status(400).json({
+        error:true,
+        message: err.message
+      })
+    });
+  });
+});
+
+app.post('/api/createpost', (req, res) => {
+  
+  const { token, uploads, service, car, description } = req.body;
+
+  function verifiedToken() {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, tokenSecret, function(err, decoded) {
+        if (err) {
+          reject(err);
+        };
+        console.log('email', decoded.email);
+        resolve(decoded.email);
+      });
+    });
+  };
+
+  function updateTokenActiveTime(token) {
+    return new Promise((resolve, reject) => {
+      connection.then(client => {
+        client
+        .db("automotiveshield")
+        .collection("tokens")
+        .updateOne({token: token}, {$set: {"activeAt": new Date()}}, (err, result) => {
+          if (err || result === null) {
+            reject(err || new Error('Token not found.'));
+          };
+          console.log('Token Active Time Updated')
+          resolve('Token Active Time Updated');
+        });
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  };
+
+  function checkDataBaseForToken(token) {
+    return new Promise((resolve, reject) => {
+      connection.then(client => {
+        client
+        .db("automotiveshield")
+        .collection("tokens")
+        .findOne({
+          token: token
+        }, (err, result) => {
+          if (err || result === null) {
+            reject(err || new Error('Token not found.'))
+          };
+          console.log('Token Found')
+          resolve('Token Found');
+        });
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  };
+
+  function moveFiles() {
+    const fs = require('fs');
+    const path = require('path');
+    let today = new Date();
+    let date = today.getDate().toString();
+    let month = today.getMonth().toString();
+    let year = today.getFullYear().toString();
+    if (date < 10) date = '0' + date;
+    if (month < 10) month = '0' + month;
+    return new Promise(async (resolve, reject) => {
+      const files = await Promise.all(uploads.map(async (file) => {
+        const oldPath = path.resolve(tmpFolder) + '/' + file.filename;
+        const videoType = ['video/mp4', 'video/quicktime'];
+        let extension = file.mimetype.split('/').pop();
+        if (extension === 'quicktime') extension = 'MOV';
+        const userPath = env === "production" ?  path.resolve('../assets/userData/') : path.resolve('./public/userData');
+        const customFolderStructure = `${userPath}/${year}/${month}/${date}/`;
+        if (!fs.existsSync(customFolderStructure)) {
+          fs.mkdirSync(customFolderStructure, { recursive: true });
+        };
+        const newPath = `${customFolderStructure}${file.filename}.${extension}`;
+        file.publicFilePath = `/userData/${year}/${month}/${date}/${file.filename}.${extension}`;
+        fs.rename(oldPath, newPath, function (err) {
+          if (err) reject(err);
+        });
+        if (videoType.includes(file.mimetype)) {
+          const oldThumbnailPath = path.resolve(tmpFolder) + '/' + file.thumbnailName;
+          const newThumbnailPath = `${customFolderStructure}${file.thumbnailName}`;
+          file.publicThumbnailPath = `/userData/${year}/${month}/${date}/${file.thumbnailName}`;
+          fs.rename(oldThumbnailPath, newThumbnailPath, function (err) {
+            if (err) reject(err);
+          });
+        };
+        return file;
+      }));
+      resolve(files);
+    });
+  };
+
+  function sendFilesToDatabase(files) {
+    return new Promise((resolve, reject) => {
+      connection.then(client => {
+        client
+        .db("automotiveshield")
+        .collection("cars")
+        .insertOne({
+          service: service,
+          car: car,
+          description: description,
+          uploads: files
+        }, (err, result) => {
+          if (err || result === null) {
+            if (err) reject(new Error('Something went wrong saving your files to the database.'));
+          };
+          resolve('Files saved to db');
+        });
+      }).catch(err => {
+        reject(err);
+      });
+    });
+  };
+
+  async function runAsync() {
+    const decodedEmail = await verifiedToken();
+    await checkDataBaseForToken(token);
+    await updateTokenActiveTime(token);
+    const files = await moveFiles();
+    await sendFilesToDatabase(files);
+    res.json({
+      message: 'Successfully uploaded files.'
     });
   };
 
